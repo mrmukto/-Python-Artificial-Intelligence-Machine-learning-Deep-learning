cedd73fbe04b9da79f79a486697ed5ea5c46a10c
Mamunur Rashid Mukto <34998258+mukto33@users.noreply.github.com>
2021-07-26 18:23:48 +0600

    This from extra java icon change

--- src/Testclass.java
+++ src/Testclass.java
@@ -3,5 +3,10 @@ public class Testclass {
 
     public static void main(String[] args) {
         System.out.println("This is My one ");
+
+        int a = 6;
+        if(a<5)
+            System.out.println("Not exac");
+        else System.out.println("correct");
     }
 }
